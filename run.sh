8bdf4fb0823d33d161cdd551afadcff3ad0c7113
CanisRetro <canisretro@gmail.com>
2022-04-25 07:44:11 -0700

    imp: remove windows eol

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ power_manager_root="$PWD"
 config_dir="$power_manager_root/config"
 log_dir="$power_manager_root/log"
 
-python3 ./src/__main__.py "$config_dir" "$log_dir"
+python ./src/__main__.py "$config_dir" "$log_dir"
